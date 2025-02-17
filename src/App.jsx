7318767c9a9ec4b74406f4ea5a9b4de2abea194f
AmrahImtiaz <amrahimtiazgame@gmail.com>
2025-02-17 23:35:22 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Cursor from "./pages/Cursor"
 import About from "./pages/About"
 import Home from "./pages/Home"
+import Showreel from "./pages/Showreel"
+import Work from "./pages/Work"
+import Slider from "./pages/Slider"
 
 const App = () => {
   return (
@@ -8,6 +11,8 @@ const App = () => {
     <Cursor/>
     <Home/>
     <About/>
+    <Showreel/>
+    <Work/>
     </>
   )
 }
