4bcaf559c566b9be76d7bfbdabbf38b1985bca9c
AmrahImtiaz <amrahimtiazgame@gmail.com>
2025-02-21 14:07:39 +0500

    tell me this works

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,50 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Work.module.css'; // Import the CSS Module
-import shanstall1 from '../../assets/shanstall1.jpg';
+
+// 3D SETS
+import shanstall1 from '../../assets/img/shanstall1.jpg';
+import shanstall2 from '../../assets/img/shanstall2.jpg';
+import nescafe1 from '../../assets/img/nescafe1.jpg';
+import pakautoshow from '../../assets/img/pakautoshow.png';
+import dollarstationery from '../../assets/img/dollarstationery.jpg';
+import hamzarashid from '../../assets/img/hamzarashid.png';
+
+// 2D MOTION GRAPHICS
+import motion2dVideo1 from '../../assets/img/TARBIYAT.mp4';
+import motion2dVideo2 from '../../assets/img/IAM.mp4';
+import motion2dVideo3 from '../../assets/img/FLIX.mp4';
+import motion2dVideo4 from '../../assets/img/PrintsPak.mp4';
+
+
+// 3D MOTION GRAPHICS
+import motion3dVideo1 from '../../assets/img/Inspector.mp4';
+import motion3dVideo2 from '../../assets/img/Lunar Eye.mp4';
+import motion3dVideo3 from '../../assets/img/Feras.mp4';
+
+// POSTS
+import waqarsalon from '../../assets/img/waqarsalon-post.jpg';
+import JummatulWida from '../../assets/img/09-Jummatul Wida.jpg';
+import Comingsoon from '../../assets/img/Coming Soon POST Design.jpg';
+
+
+const projects = [
+  { src: shanstall2, title: "Smart Watch", category: "3D Set", type: "image" },
+  { src: shanstall1, title: "Camera Film", category: "3D Set", type: "image" },
+  { src: pakautoshow, title: "Coffee", category: "3D Set", type: "image" },
+  { src: nescafe1, title: "Phone", category: "3D Set", type: "image" },
+  { src: dollarstationery, title: "Keyboard", category: "3D Set", type: "image" },
+  { src: hamzarashid, title: "Wrist Watch", category: "3D Set", type: "image" },
+  { src: motion2dVideo1, title: "2D Motion 1", category: "2D Motion Graphic", type: "video" },
+  { src: motion2dVideo2, title: "2D Motion 2", category: "2D Motion Graphic", type: "video" },
+  { src: motion2dVideo3, title: "2D Motion 3", category: "2D Motion Graphic", type: "video" },
+  { src: motion2dVideo4, title: "2D Motion 4", category: "2D Motion Graphic", type: "video" },
+  { src: motion3dVideo1, title: "3D Motion 1", category: "3D Motion Graphic", type: "video" },
+  { src: motion3dVideo2, title: "3D Motion 2", category: "3D Motion Graphic", type: "video" },
+  { src: motion3dVideo3, title: "3D Motion 3", category: "3D Motion Graphic", type: "video" },
+  { src: waqarsalon, title: "Waqar Salon", category: "Posts", type: "image" },
+  { src: JummatulWida, title: "Jummatul Wida", category: "Posts", type: "image" },
+  { src: Comingsoon, title: "Kon Banaye Ga Star", category: "Posts", type: "image" },
+];
 
 const Work = () => {
+  const [filter, setFilter] = useState("All");
+
+  // Function to get one project from each category
+  const getUniqueProjectsByCategory = () => {
+    const uniqueCategories = new Set();
+    return projects.filter(item => {
+      if (!uniqueCategories.has(item.category)) {
+        uniqueCategories.add(item.category);
+        return true;
+      }
+      return false;
+    });
+  };
+
+  const filteredProjects = filter === "All" ? getUniqueProjectsByCategory() : projects.filter(item => item.category === filter);
+
   return (
     <>
-      {/* Navigation */}
-      <nav className={styles.nav}>
-        <div className={styles.container}>
-          <div className={styles.grid}>
-            <div className={`${styles['column-xs-12']} ${styles['column-md-10']}`}>
-              <p id={styles.highlight}>Katherine Kato</p>
-            </div>
-            <div className={`${styles['column-xs-12']} ${styles['column-md-2']}`}>
-              <ul>
-                <li><a href="#" className={styles.active}>Work</a></li>
-                <li><a href="#">About</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <h1 className='text-5xl text-white font-bold text-center mb-10 mt-10'>3D SETS</h1>
+      
+      {/* Filter Buttons */}
+      <div className='flex justify-center space-x-4 mb-10'>
+        {["All", "3D Set", "2D Motion Graphic", "3D Motion Graphic", "Posts"].map(category => (
+          <button
+            key={category}
+            className={`px-4 py-2 rounded-lg text-white ${filter === category ? 'bg-red-300' : 'bg-gray-700'}`}
+            onClick={() => setFilter(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
 
       {/* Gallery Section */}
       <section className={styles.gallery}>
         <div className={styles.container}>
           <div className={styles.grid}>
-            {/* Gallery Items */}
-            {[
-              { src: 'https://i.pinimg.com/736x/27/31/3f/27313f82dff0a14d8ff844ca1c035b06.jpg', title: "Smart Watch" },
-              { src: "https://assets.codepen.io/1159990/camera-film.jpg", title: "Camera Film" },
-              { src: "https://assets.codepen.io/1159990/coffee.jpg", title: "Coffee" },
-              { src: "https://assets.codepen.io/1159990/phone.jpg", title: "Phone" },
-              { src: "https://assets.codepen.io/1159990/keyboard.jpg", title: "Keyboard" },
-              { src: "https://assets.codepen.io/1159990/wrist-watch.jpg", title: "Wrist Watch" },
-            ].map((item, index) => (
+            {filteredProjects.map((item, index) => (
               <div key={index} className={`${styles['column-xs-12']} ${index < 3 ? styles['column-md-4'] : index < 5 ? styles['column-md-6'] : styles['column-xs-12']}`}>
                 <figure className={styles['img-container']}>
-                  <img src={item.src} alt={item.title} />
+                  {item.type === "image" ? (
+                    <img src={item.src} alt={item.title} />
+                  ) : (
+                    <video controls autoPlay loop muted>
+                      <source src={item.src} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  )}
                   <figcaption className={styles['img-content']}>
                     <h2 className={styles.title}>{item.title}</h2>
-                    <h3 className={styles.category}>Showcase</h3>
+                    <h3 className={styles.category}>{item.category}</h3>
                   </figcaption>
                   <span className={styles['img-content-hover']}>
                     <h2 className={styles.title}>{item.title}</h2>
-                    <h3 className={styles.category}>Showcase</h3>
+                    <h3 className={styles.category}>{item.category}</h3>
                   </span>
                 </figure>
               </div>
@@ -53,21 +110,10 @@ const Work = () => {
         </div>
       </section>
 
-      {/* Footer */}
-      <footer className={styles.footer}>
-        <div className={styles.container}>
-          <div className={styles.grid}>
-            <div className={styles['column-xs-12']}>
-              <ul className={styles.social}>
-                <li><a href="https://twitter.com/kato_katherine" target="_blank" rel="noopener noreferrer"><span className="fab fa-twitter"></span></a></li>
-                <li><a href="https://codepen.io/kathykato" target="_blank" rel="noopener noreferrer"><span className="fab fa-codepen"></span></a></li>
-                <li><a href="https://github.com/kathykato" target="_blank" rel="noopener noreferrer"><span className="fab fa-github"></span></a></li>
-              </ul>
-              <p className={styles.copyright}>&copy; Copyright 2018 Katherine Kato</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      {/* Centered MORE Button */}
+      <div className="flex justify-center mt-10">
+        <button className='font-bold text-2xl rounded-full bg-white text-center px-6 py-3'>MORE</button>
+      </div>
     </>
   );
 };
